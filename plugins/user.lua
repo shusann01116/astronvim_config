a0baecdd9f6e819915684f586458eb7551870afa
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-06-10 13:14:54 +0900

    Add lsp signature plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,13 +2,11 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+  {
+    "ray-x/lsp_signature.nvim",
+    event = "BufRead",
+    config = function() require("lsp_signature").setup() end,
+  },
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
