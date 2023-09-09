bbe675ec037748e64fed617a5dfd9361165bfb17
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2023-09-10 00:48:59 +0900

    feat: add usercommand to install all packages for headless mode

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,29 +1,65 @@
+local lspconfigs = {
+  "lua_ls",
+  "gopls",
+  "taplo",
+  "omnisharp",
+  "yamlls",
+  "dockerls",
+  "bashls",
+  "dartls",
+  "gopls",
+  "jsonls",
+  "marksman",
+  "pyright",
+  "ruff_lsp",
+  "rust_analyzer",
+  "terraformls",
+  "yamlls",
+}
+local null_lss = {
+  "prettierd",
+  "stylua",
+  "actionlint",
+  "hadolint",
+  "shellcheck",
+  "shfmt",
+  "gomodifytags",
+  "gofumpt",
+  "iferr",
+  "impl",
+  "goimports",
+  "black",
+  "isort",
+  "tflint",
+  "tfsec",
+}
+local daps = {
+  "codelldb",
+  "python",
+  "netcoredbg",
+  "bash",
+  "dart",
+  "delve",
+}
+
 -- customize mason plugins
 return {
+  vim.api.nvim_create_user_command(
+    "MasonInstallAll",
+    function()
+      vim.cmd("LspInstall " .. table.concat(lspconfigs, " "))
+      vim.cmd("NullLsInstall " .. table.concat(null_lss, " "))
+      vim.cmd("DapInstall " .. table.concat(daps, " "))
+    end,
+    {}
+  ),
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
-        "gopls",
-        "taplo",
-        "omnisharp",
-        "yamlls",
-        "dockerls",
-        "bashls",
-        "dartls",
-        "gopls",
-        "jsonls",
-        "marksman",
-        "pyright",
-        "ruff_lsp",
-        "rust_analyzer",
-        "terraformls",
-        "yamlls",
-      })
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, lspconfigs)
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -32,23 +68,7 @@ return {
     -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "prettierd",
-        "stylua",
-        "actionlint",
-        "hadolint",
-        "shellcheck",
-        "shfmt",
-        "gomodifytags",
-        "gofumpt",
-        "iferr",
-        "impl",
-        "goimports",
-        "black",
-        "isort",
-        "tflint",
-        "tfsec",
-      })
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, null_lss)
     end,
   },
   {
@@ -56,14 +76,7 @@ return {
     -- overrides `require("mason-nvim-dap").setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "codelldb",
-        "python",
-        "netcoredbg",
-        "bash",
-        "dart",
-        "delve",
-      })
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, daps)
     end,
   },
 }
