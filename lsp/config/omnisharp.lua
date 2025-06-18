5cafee7cfc80faf3a21a0e381c5910b808b6b725
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-06-19 01:27:20 +0900

    Migrate to V4

--- lsp/config/omnisharp.lua
+++
@@ -1,9 +0,0 @@
-return {
-  handlers = {
-    ["textDocument/definition"] = require("omnisharp_extended").handler,
-  },
-  enable_roslyn_analyzers = true,
-  organize_imports_on_format = true,
-  enable_import_completion = true,
-  enable_decompilation_support = true,
-}
---
+++ lua/community.lua
@@ -0,0 +1,17 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.python" },
+}
