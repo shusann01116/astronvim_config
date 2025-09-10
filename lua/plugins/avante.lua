78863f7b48e544e806c2a0fcfb06790d918eda7a
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-09-10 17:39:47 +0900

    Remove auto_suggestions from avante

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,13 +1,9 @@
 ---@type LazySpec
 return {
-	"zbirenbaum/copilot.lua",
 	{
 		"yetone/avante.nvim",
 		opts = function(_, opts)
 			return require("astrocore").extend_tbl(opts, {
-				behavior = {
-					auto_suggestions = true,
-				},
 				mappings = {
 					submit = {
 						insert = "<C-a>",
