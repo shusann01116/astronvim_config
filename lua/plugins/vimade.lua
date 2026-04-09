43e940dcd39547a2b46abd55b047d6836f6e1e88
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2026-04-09 12:09:45 +0900

    Add vimade plugin for inactive pane and window dimming

---
+++ lua/plugins/vimade.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+	"tadaa/vimade",
+	event = "UIEnter",
+	opts = {
+		fadelevel = 0.4,
+		enablefocusfading = true,
+		ncmode = "buffers",
+		groupdiff = true,
+		groupscrollbind = false,
+	},
+}
